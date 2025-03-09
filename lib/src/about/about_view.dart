59147f5d979be01befb298c239f060fbfaa6225e
TheVezz <37942423+TheVezz@users.noreply.github.com>
2025-03-09 16:19:38 +0100

    update

--- lib/src/about/about_view.dart
+++ lib/src/about/about_view.dart
@@ -4,22 +4,23 @@ import 'package:url_launcher/url_launcher.dart';
 import '../core/logger.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-///Page information of the app.
-///contains links to Nexus Mods, Github and Play Store.
+/// Page information of the app.
+/// Contains links to Nexus Mods, Github, Play Store, and Ko-Fi support.
 class AboutView extends StatelessWidget {
   const AboutView({super.key});
 
-  // Link you have some sites
+  // Links to various platforms
   final String nexusModsUrl = "https://www.nexusmods.com/callofdutyblackops6/mods/7?tab=description";
   final String githubUrl = "https://github.com/TheVezz/BlackOps6Tools/tree/main";
   final String playStoreUrl = "";
+  final String koFiUrl = "https://ko-fi.com/Z8Z11BPKE0";
 
-  ///Function to open a URL in the browser
+  /// Function to open a URL in the browser
   Future<void> _launchUrl(String url) async {
     if (await canLaunchUrl(Uri.parse(url))) {
       await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
     } else {
-      logger.warning('Impossible open the link: $url');
+      logger.warning('Impossible to open the link: $url');
     }
   }
 
@@ -45,7 +46,7 @@ class AboutView extends StatelessWidget {
           ),
           const SizedBox(height: 16),
           
-          //Link a Nexus Mods
+          // Link to Nexus Mods
           ListTile(
             leading: SvgPicture.asset(
               'assets/images/platform/nexusmods.svg',
@@ -58,7 +59,7 @@ class AboutView extends StatelessWidget {
           ),
           const Divider(),
           
-          // Link al Play Store
+          // Link to Play Store
           ListTile(
             leading: SvgPicture.asset(
               'assets/images/platform/playstore.svg',
@@ -71,7 +72,7 @@ class AboutView extends StatelessWidget {
           ),
           const Divider(),
 
-          // Link a GitHub
+          // Link to GitHub
           ListTile(
             leading: SvgPicture.asset(
               'assets/images/platform/github.svg',
@@ -87,6 +88,21 @@ class AboutView extends StatelessWidget {
           Text(
             AppLocalizations.of(context)!.aboutDescription3,
             style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          const SizedBox(height: 24),
+          
+          // Ko-Fi Support Section
+          Text(
+            "Support Me on Ko-Fi",
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          const SizedBox(height: 12),
+          InkWell(
+            onTap: () => _launchUrl(koFiUrl),
+            child: Image.asset(
+              'assets/images/kofi.webp', // Scarica e salva l'immagine in assets
+              height: 36,
+            ),
           ),
         ],
       ),
